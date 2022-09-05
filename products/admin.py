4cafaf85d6b10db62216e58b44493072d469ab23
mehdi <mpourbaba@gmail.com>
2022-09-05 22:53:57 +0430

    add products_category too products app and set CategoryAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    search_fields = ['title', 'created_at']
--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', related_name='children',
+                                on_delete=models.CASCADE,
+                                null=True, blank=True)
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, help_text='You can leave it blank')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):                           
+        full_path = [self.title]             
+        parent_name = self.parent
+        while parent_name is not None:
+            full_path.append(parent_name.title)
+            parent_name = parent_name.parent
+        return ' -> '.join(full_path[::-1])
