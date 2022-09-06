6120bfae73fbfc47f7a0cb5aa45ab91d6b027b07
mehdi <mpourbaba@gmail.com>
2022-09-06 21:44:16 +0430

    add products model and set ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_at']
     search_fields = ['title', 'created_at']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'created_at', 'active']
--- products/models.py
+++ products/models.py
@@ -25,3 +25,26 @@ class Category(models.Model):
             full_path.append(parent_name.title)
             parent_name = parent_name.parent
         return ' -> '.join(full_path[::-1])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, help_text='You can leave it blank')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    cover = models.ImageField(upload_to='products_cover/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('slug', 'title',)
+
+    def __str__(self):
+        return self.title
+
