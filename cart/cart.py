5de2a1f53f33d40938f65435021e74d2414dbb80
mehdi <mpourbaba@gmail.com>
2022-12-17 21:20:34 +0330

    context_processors added to cart app

--- cart/cart.py
+++ cart/cart.py
@@ -71,7 +71,7 @@ class Cart:
             yield item
 
     def __len__(self):
-        return len(self.cart.keys())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session['cart']
